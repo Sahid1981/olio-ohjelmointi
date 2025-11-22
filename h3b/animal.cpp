104fc56d083fb7a07a59fa037fc3bc554f565b2e
Juha Jermalainen <juha.jermalainen@gmail.com>
2025-11-22 13:54:58 +0200

    Ensimmäinen commit

---
+++ h3b/animal.cpp
@@ -0,0 +1,9 @@
+#include "animal.h"
+#include<iostream>
+
+Animal::Animal() {}
+
+Animal::callout()
+{
+    std::cout << "Eläin ääntelee." << std:endl;
+}
---
+++ h3b/dog.cpp
@@ -0,0 +1,9 @@
+#include "dog.h"
+#include <iostream>
+
+Dog::Dog() {}
+
+Dog::callout()
+{
+    std::cout << "Koira haukkuu." << std::endl;
+}
